4d9831ea11be15819b864e622f343c8c38fd0716
KimJW <fresh04502@naver.com>
2025-05-13 14:21:05 +0900

    Update UserSaveDto.java
    user Role 타입 입력 수정

--- user-service/src/main/java/com/playdata/userservice/user/dto/UserSaveDto.java
+++ user-service/src/main/java/com/playdata/userservice/user/dto/UserSaveDto.java
@@ -30,7 +30,7 @@ public class UserSaveDto {
               .email(this.email)
               .password(this.password)
               .username(this.username)
-              .role(Role.USER)
+              .role(Role.valueOf(this.role.name()))
               .build();
    }
 

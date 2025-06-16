55d2743728bb0d7633f60d9c9a4377a33edb9c44
KimJW <fresh04502@naver.com>
2025-06-16 09:22:41 +0900

    Update EvalClient.java

--- course-service/src/main/java/com/springstudy/courseservice/client/EvalClient.java
+++ course-service/src/main/java/com/springstudy/courseservice/client/EvalClient.java
@@ -3,17 +3,14 @@ package com.springstudy.courseservice.client;
 import com.springstudy.courseservice.common.dto.CommonResDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Map;
 
-
-// course 브랜치를 병합받았는데, 선인이 되어있지 않아서, 따로 만든 클라이언트 입니다!
 @FeignClient(name = "eval-service")
 public interface EvalClient {
 
-    @PostMapping("eval/course-eval-rating-feign")
-    CommonResDto<Map<Long, Double>> findCoursesRatingFeign(List<Long> productIds);
-
-
+    @PostMapping("/eval/course-eval-rating-feign")
+    CommonResDto<Map<Long, Double>> findCoursesRatingFeign(@RequestBody List<Long> prodIdList);
 }
